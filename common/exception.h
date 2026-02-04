1bcab241e966a1211c9ebd60b35e73ffbee80911
Wang Zhiyong <zhiyong4wang@foxmail.com>
2026-02-04 17:56:37 +0800

    Add common exception helpers

---
+++ common/exception.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+namespace common {
+
+class Exception : public std::runtime_error {
+ public:
+  Exception(const char *type, std::string message, const char *file, int line,
+            const char *function)
+      : std::runtime_error(BuildWhat(type, message, file, line, function)),
+        type_(type ? type : "Exception"),
+        message_(std::move(message)),
+        file_(file ? file : ""),
+        function_(function ? function : ""),
+        line_(line) {}
+
+  const std::string &type() const noexcept { return type_; }
+  const std::string &message() const noexcept { return message_; }
+  const std::string &file() const noexcept { return file_; }
+  const std::string &function() const noexcept { return function_; }
+  int line() const noexcept { return line_; }
+
+ private:
+  static std::string BuildWhat(const char *type, const std::string &message,
+                               const char *file, int line,
+                               const char *function) {
+    std::string out;
+    const char *type_name = type ? type : "Exception";
+    out.append(type_name);
+    if (!message.empty()) {
+      out.append(": ");
+      out.append(message);
+    }
+    if (file && *file) {
+      out.append(" @ ");
+      out.append(file);
+      out.push_back(':');
+      out.append(std::to_string(line));
+      if (function && *function) {
+        out.push_back(' ');
+        out.append(function);
+      }
+    }
+    return out;
+  }
+
+  std::string type_;
+  std::string message_;
+  std::string file_;
+  std::string function_;
+  int line_ = 0;
+};
+
+#define DEFINE_EXCEPTION(name, base)                           \
+  class name : public base {                                   \
+   public:                                                     \
+    name(std::string message, const char *file, int line,      \
+         const char *function)                                \
+        : base(#name, std::move(message), file, line, function) {} \
+  }
+
+DEFINE_EXCEPTION(InvalidArgumentError, Exception);
+DEFINE_EXCEPTION(NotFoundError, Exception);
+DEFINE_EXCEPTION(InternalError, Exception);
+
+}  // namespace common
+
+#define THROW(exception_type, message) \
+  throw exception_type((message), __FILE__, __LINE__, __func__)
+
+#define THROW_IF(condition, exception_type, message) \
+  do {                                               \
+    if (condition) {                                 \
+      THROW(exception_type, message);                \
+    }                                                \
+  } while (0)
+
+#define CHECK(condition, exception_type, message) \
+  do {                                            \
+    if (!(condition)) {                           \
+      THROW(exception_type, message);             \
+    }                                             \
+  } while (0)
