e96cc81b1888f76a6642aa58e7bb1aa3ed2aaeec
Wang Zhiyong <zhiyong4wang@foxmail.com>
2026-09-07 15:25:49 +0800

    refactor(planner): remove redundant aggregation rewrite

--- ir/logical_plan.h
+++ ir/logical_plan.h
@@ -98,7 +98,6 @@ struct LogicalPrecomputedExpression {
 
 struct LogicalProjectionItem {
   const ast::Expression *expression = nullptr;
-  std::shared_ptr<ast::Expression> owned_expression;
   std::string alias;
   ast::SemanticVariableType semantic_type = ast::SemanticVariableType::kUnknown;
   std::vector<LogicalPrecomputedExpression> precomputed_expressions;
